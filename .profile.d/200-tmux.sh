59fb3dd17012460f139c09883bcb48d3a4fa162b
Guy Hughes <g@gxg.me>
2014-05-16 14:58:12 -0400

    up

--- .profile.d/200-tmux.sh
+++ .profile.d/200-tmux.sh
@@ -1,5 +1,6 @@
 
 alias tmux="tmux -f ~/.config/tmux/conf"
 mux() { [[ -z "$TMUX" ]] && { tmux attach -d || tmux ;} }
-shux() { ssh "$1" -t tmux a -d ;}
+shux() { ssh "$1" -t tmux a -d;}
+smux() { ssh "$1" -t tmux new-session -t1 || tmux new-session -s1;}
 
