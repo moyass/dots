3cbebe2c012aa04e1f5e55c57c78f72f29a8dc40
Guy Hughes <g@gxg.me>
2014-05-24 17:49:35 -0400

    tmuxstat

--- .profile.d/200-tmux.sh
+++ .profile.d/200-tmux.sh
@@ -4,4 +4,3 @@ shux() { ssh "$1" -t tmux a -d;}
 smux() { ssh $* -t 'exec ~/bin/onemux';}
 
 
-[[ -n "$SSH_CONNECTION" ]] && tmux set-option -g status-right '#[default]#(whoami)#[fg=black]@#[fg=colour166]#[bold]#{host}'
