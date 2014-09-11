48baf8b0194b1fe0bea1253be63170b0724b34b8
Guy Hughes <g@gxg.me>
2014-09-11 04:02:37 -0400

    modified:   900-hostname-file.sh 	renamed:    hostnames/includes/drush.complete.sh -> after-shell-specific/drush.complete.sh 	deleted:    hostnames/ohec.sh

--- .profile.d/900-hostname-file.sh
+++ .profile.d/900-hostname-file.sh
@@ -4,10 +4,25 @@
 
 # WARN: Echoing feedback causes ssh cloud apps to fail. Sad.
 
-if [ -f ~/.profile.d/hostnames/`hostname`.sh ]; then
-   source ~/.profile.d/hostnames/`hostname`.sh
+source_if_exists {
+  if [ -f $1 ]; then
+    source $1
+  fi
+
+}
+
+THEHOSTNAME=`hostname`
+THESHORTHOSTNAME=`hostname -s`
+
+# Sometimes `hostname` is fqdn, sometimes not. 
+# This is anonying.
+# So just check for both.
+
+if [ $THEHOSTNAME != $THESHORTHOSTNAME   ]; then
+  source_if_exists ~/.profile.d/hostnames/${THESHORTHOSTNAME}.sh
+  source_if_exists ~/.profile.d/hostnames/${THESHORTHOSTNAME}.private.sh
 fi
 
-if [ -f ~/.profile.d/hostnames/`hostname`.private.sh ]; then
-   source ~/.profile.d/hostnames/`hostname`.private.sh
-fi
+source_if_exists ~/.profile.d/hostnames/${THEHOSTNAME}.sh
+source_if_exists ~/.profile.d/hostnames/${THEHOSTNAME}.private.sh
+
--- .profile.d/hostnames/ohec.sh
+++
@@ -1,1 +0,0 @@
-source includes/drush.complete.sh
