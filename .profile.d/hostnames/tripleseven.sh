91f398369f56c39ec7b36a9a16504fdd911e5765
Guy Hughes <g@gxg.me>
2014-09-13 11:16:29 -0400

    move askpass to hostnamefile from xinitrc

--- .profile.d/hostnames/tripleseven.sh
+++ .profile.d/hostnames/tripleseven.sh
@@ -1,5 +1,8 @@
 export TERMINAL="urxvtc"
 
+export SSH_ASKPASS="/usr/bin/ssh-askpass"
+export SUDO_ASKPASS="/usr/bin/ssh-askpass"
+
 PERL_MB_OPT="--install_base \"/home/hugg/perl5\""; export PERL_MB_OPT;
 PERL_MM_OPT="INSTALL_BASE=/home/hugg/perl5"; export PERL_MM_OPT;
 
