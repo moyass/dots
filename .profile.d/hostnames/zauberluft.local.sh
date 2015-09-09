3ef43a32f96e59f4ee35e785ef56d05bf28cde4a
Guy Hughes <g@gxg.me>
2015-09-08 23:51:21 -0400

    zauberluft pre-kill

--- .profile.d/hostnames/zauberluft.local.sh
+++ .profile.d/hostnames/zauberluft.local.sh
@@ -3,4 +3,6 @@ export CURL_CA_BUNDLE='/Users/hugg/.weechat/ssl/ca-bundle.crt'
 # Add mysql to PATH
 export PATH="/usr/local/mysql/bin:$PATH"
 
+eval "$(rbenv init -)"
 
+export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
