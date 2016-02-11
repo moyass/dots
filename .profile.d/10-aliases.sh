c0824ba9289d23470a948051d64c752711fbb584
Guy Hughes <g@gxg.me>
2016-02-11 17:12:21 -0500

    extract() is gone, finally. plagurized, never used, wasted. ☺

--- .profile.d/10-aliases.sh
+++ .profile.d/10-aliases.sh
@@ -126,31 +126,6 @@ aes_encypt () {
 aes_decrypt () {
   openssl enc -aes-256-cbc -d -in $1 -out "${1%.*}"
 } #}}}
-#{{{1 Extract
-extract() {
-  # TODO: remove this or improve it
-  if [ -f $1 ]; then
-    case $1 in
-      *.tar.bz2)   tar xvjf $1    ;;
-      *.tar.gz)    tar xvzf $1    ;;
-      *.tar.xz)    tar xvJf $1    ;;
-      *.bz2)       bunzip2 $1     ;;
-      *.rar)       unrar x $1     ;;
-      *.gz)        gunzip $1      ;;
-      *.tar)       tar xvf $1     ;;
-      *.tbz2)      tar xvjf $1    ;;
-      *.tgz)       tar xvzf $1    ;;
-      *.zip)       unzip $1       ;;
-      *.Z)         uncompress $1  ;;
-      *.7z)        7z x $1        ;;
-      *.xz)        unxz $1        ;;
-      *.exe)       cabextract $1  ;;
-      *)           echo "\`$1': unrecognized file compression" ;;
-    esac
-  else
-    echo "\`$1' is not a valid file"
-  fi
-}
 #{{{1 GNU Find
 set_finder() {
   FIND="find"
